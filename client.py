1438459e831aa1c0d2dcebbb248dad983d121b31
Muneeb Saddal <muneebsaddal@outlook.com>
2018-12-08 10:15:50 +0500

    Functions for download with multiple connections
    Using multithreading downloading file over multiple connections if byte ranges is supported.
    Downloaded test files also included

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import sys, getopt
+import sys, getopt, requests, threading 
 
 def argsInput(argv):
    numConnection = ''
@@ -38,4 +38,62 @@ def argsInput(argv):
    print 'f', fileLocation
    print 'o', outputLocation
 
-argsInput(sys.argv[1:])
+
+fileLocation = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+file_name = fileLocation.split('/')[-1]
+file_size = 0
+numConnection = 4
+
+
+def handlerURL(start, end, url, filename): 
+   
+   # specify the starting and ending of the file 
+   headers = {'Range': 'bytes=%d-%d' % (start, end)} 
+
+   # request the specified part and get into variable  
+   r = requests.get(url, headers=headers, stream=True) 
+
+   # open the file and write the content of the html page 
+   # into file. 
+   with open(filename, "r+b") as fp: 
+      fp.seek(start) 
+      var = fp.tell()
+      fp.write(r.content) 
+
+def downloadFileURL(): 
+   r = requests.head(fileLocation) 
+   statusCode = r.status_code 
+   print statusCode
+   if statusCode == 404:
+      print 'File not found'
+      sys.exit()
+   elif statusCode == 200:
+      numConnection = 1
+   try: 
+      file_size = int(r.headers['content-length']) 
+   except: 
+      print "Invalid URL"
+      return
+   part = int(file_size) / numConnection 
+   fp = open(file_name, "wb") 
+   fp.write('\0' * file_size) 
+   fp.close() 
+   for i in range(numConnection): 
+      start = part * i 
+      end = start + part 
+      # create a Thread with start and end locations 
+      t = threading.Thread(target=handlerURL, 
+         kwargs={'start': start, 'end': end, 'url': fileLocation, 'filename': file_name}) 
+      t.setDaemon(True) 
+      t.start() 
+   main_thread = threading.current_thread() 
+   for t in threading.enumerate(): 
+      if t is main_thread: 
+         continue
+      t.join() 
+   print '%s downloaded' % file_name 
+
+
+
+#argsInput(sys.argv[1:])
+downloadFileURL()
